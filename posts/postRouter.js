26db1605081e27da9d8b024f3e127e533db7c11a
amlane <twelvexstring@gmail.com>
2019-05-29 18:02:07 -0500

    get request for post database is working

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,12 +2,18 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+const postDb = require('./postDb.js');
 
+router.get('/', (req, res) => {
+    postDb.get().then( posts => {
+        res.status(200).json(posts)
+    }).catch(error => {
+        res.status(500).json({ message: "Error trying to get posts from the database." })
+    })
 });
 
 router.get('/:id', (req, res) => {
-
+    const id = req.params.id;
 });
 
 router.delete('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(logger);
 
 
 server.use('/api/users', userRouter)
+server.use('/api/posts', postRouter)
 
 
 server.get('/', (req, res) => {

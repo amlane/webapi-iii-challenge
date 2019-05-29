e60e5885288f1b330d194d6ef3d44be804f2eceb
amlane <twelvexstring@gmail.com>
2019-05-29 14:55:25 -0500

    got logger set up and tested

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
+
+server.use(express.json());
+server.use(logger);
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +13,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-
+  console.log(`a request was made from ${req.method} request to ${req.url}`);
+  next();
 };
 
 module.exports = server;

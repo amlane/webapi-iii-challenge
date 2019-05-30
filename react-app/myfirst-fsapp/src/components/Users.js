2d471b6a6a66b8664012d8aadfd936d6075d2f1a
amlane <twelvexstring@gmail.com>
2019-05-30 01:22:17 -0500

    adding react app

---
+++ react-app/myfirst-fsapp/src/components/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Users extends React.Component{
+
+    render(){
+        return (
+            <div>Yo dawg I'm the Users Component</div>
+        )
+    }
+}
+
+export default Users;

9d830d3a0bfdd2dcf9d8d89837787fdcbf7fd371
amlane <twelvexstring@gmail.com>
2019-05-30 01:43:09 -0500

    researched cors and got a response in the console

--- react-app/myfirst-fsapp/src/components/Users.js
+++ react-app/myfirst-fsapp/src/components/Users.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import axios from 'axios';
 
 class Users extends React.Component{
+    state = {
+        users: []
+    }
+
+    componentDidMount(){
+        axios
+        .get('http://localhost:5000/api/users')
+        .then( res => {
+            console.log(res)
+        })
+        .catch( err => {
+            console.log("error: ", err)
+        })
+    }
 
     render(){
         return (
